63e69a15e2d9f6bb27fbc016376470b070c00304
Øyvind Bratne <oyvind.b.79@gmail.com>
2021-05-28 09:33:39 +0200

    New header in script

--- git-pr.py
+++ git-pr.py
@@ -1,3 +1,21 @@
+# Copyright 2021 Øyvind Bratne
+
+# This file is part of Git-pr
+#
+# Git-pr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Git-pr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Git-pr.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import argparse
 import logging
 import subprocess
